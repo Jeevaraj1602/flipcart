8a0de79d7eab98df9789348479d1bc8dbf2874d8
Jeevaraj1602 <jeevaraj1627@gmail.com>
2026-09-04 17:46:09 +0530

    Added Docker configuration

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def place_order():
     return render_template('order_success.html', payment_method=payment_method)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+    
